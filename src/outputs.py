fa34c4f2354f650892271656c27436f6a648c1de
tantsiura <tantsiura.e@yandex.ru>
2023-10-15 12:46:47 +0300

    hw

---
+++ src/outputs.py
@@ -0,0 +1,47 @@
+import csv
+import datetime as dt
+import logging
+
+from prettytable import PrettyTable
+
+from constants import DATETIME_FORMAT, RESULTS_DIR
+
+
+def control_output(results, cli_args):
+    '''Контроль вывода результатов парсинга.'''
+    output = cli_args.output
+    if output == 'pretty':
+        pretty_output(results)
+    elif output == 'file':
+        file_output(results, cli_args)
+    else:
+        default_output(results)
+
+
+def default_output(results):
+    '''Вывод данных в терминал построчно.'''
+    for row in results:
+        print(*row)
+
+
+def pretty_output(results):
+    '''Вывод данных в формате PrettyTable.'''
+    table = PrettyTable()
+    table.field_names = results[0]
+    table.align = 'l'
+    table.add_rows(results[1:])
+    print(table)
+
+
+def file_output(results, cli_args):
+    '''Создание директории и запись данных в файл.'''
+    RESULTS_DIR.mkdir(exist_ok=True)
+    parser_mode = cli_args.mode
+    now = dt.datetime.now()
+    now_formatted = now.strftime(DATETIME_FORMAT)
+    file_name = f'{parser_mode}_{now_formatted}.csv'
+    file_path = RESULTS_DIR / file_name
+    with open(file_path, 'w', encoding='utf-8') as f:
+        writer = csv.writer(f, dialect='unix')
+        writer.writerows(results)
+    logging.info(f'Файл с результатами был сохранён: {file_path}')
---
+++ src/utils.py
@@ -0,0 +1,32 @@
+import logging
+
+from bs4 import BeautifulSoup
+from requests import RequestException
+
+from exceptions import ParserFindTagException
+
+
+def get_response(session, url):
+    '''Перехват ошибки RequestException.'''
+    try:
+        response = session.get(url)
+        response.encoding = 'utf-8'
+        return response
+    except RequestException:
+        raise ConnectionError(f'Возникла ошибка при загрузке страницы {url}')
+
+
+def find_tag(soup, tag, attrs=None):
+    '''Перехват ошибки поиска тегов.'''
+    searched_tag = soup.find(tag, attrs=(attrs or {}))
+    if searched_tag is None:
+        error_msg = f'Не найден тег {tag} {attrs}'
+        logging.error(error_msg, stack_info=True)
+        raise ParserFindTagException(error_msg)
+    return searched_tag
+
+
+def get_soup(session, url):
+    response = get_response(session, url)
+    soup = BeautifulSoup(response.text, features='lxml')
+    return soup
